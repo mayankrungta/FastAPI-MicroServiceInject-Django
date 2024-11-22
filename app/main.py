cc3d25e676120b93cf483cc1e969e727797c539f
mayankrungta <mr.mynk@gmail.com>
2024-11-22 15:52:04 +0530

    Add a path param for the user detail api with unit test

--- app/main.py
+++ app/main.py
@@ -21,3 +21,16 @@ async def home_view(request: Request):
 @app.post("/")
 async def home_detail_view():
     return {"msg": "FooBar"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get('/users/{user_id}', response_class=HTMLResponse)
+async def user_detail_view(request: Request, user_id: str):
+    # user_id = kwargs['user_id']
+    print(request)
+    print(f'user_id: {user_id}')
+    return templates.TemplateResponse(request, "home.html", context={"person": user_id})
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -17,3 +17,19 @@ def test_post_home():
     assert response.status_code == 200
     assert "application/json" in response.headers['content-type']
     assert response.json() == {"msg": "FooBar"}
+
+
+def test_get_item():
+    item = 17
+    response = client.get(f"/items/{item}")
+    assert response.status_code == 200
+    assert "application/json" in response.headers['content-type']
+    assert response.json() == {"item_id": item}
+
+
+def test_user_detail():
+    user = 'Jake'
+    response = client.get(f"/users/{user}")
+    assert response.status_code == 200
+    assert "text/html" in response.headers['content-type']
+    assert f"<h1>Howdie {user}!</h1>" in response.text
